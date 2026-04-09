acdcd299d49c7712672e7a1cf7c6dddb03053376
liellnima <julia.kaltenborn@mail.mcgill.ca>
2026-04-09 19:00:47 +0200

    add output dir

--- src/__init__.py
+++ src/__init__.py
@@ -5,3 +5,4 @@ PROJECT_ROOT = APP_ROOT.parent.parent
 CONFIGS_PATH = PROJECT_ROOT / "configs"
 DATA_DIR = PROJECT_ROOT / "data"
 SCRIPTS_DIR = PROJECT_ROOT / "scripts"
+OUTPUT_DIR = PROJECT_ROOT / "output"
