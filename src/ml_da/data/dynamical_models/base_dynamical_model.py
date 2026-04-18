update for 1 step approach, fix jacobian of model

src/ml_da/data/dynamical_models/base_dynamical_model.py
@@ -7,6 +7,7 @@ import xarray as xr
 
 from ml_da.data.transformations import add_noise
 from ml_da.tools.config import DynamicalModelConfig
+from ml_da.tools.utils import get_state
 
 logger = logging.getLogger(__name__)
 
@@ -27,7 +28,7 @@ class DynamicalModel(ABC):
         # for most methods we either need ensembles or tlms
         # this can be adapted in the future, but we need to rework the whole system in that case
         # which is why I want this to throw an error, so we notice
-        if (self.ensemble_size == 1) and return_tlm:
+        if (self.ensemble_size > 1) and return_tlm:
             raise ValueError("Dynamical Models currently supports ensembles generation xor TLMs, but not both.")
         self.return_tlm = return_tlm
 
@@ -108,15 +109,10 @@ class DynamicalModel(ABC):
 
         Can do this for ensembles or single models.
         """
-
-        # TODO check this one:
-        def _func_get_single_state(dataset):
-            return dataset.isel(time=-1).to_array().data.flatten()
-
         if isinstance(full_trajectory, list):
-            return [_func_get_single_state(ds) for ds in full_trajectory]
+            return [get_state(ds, time=-1) for ds in full_trajectory]
         else:
-            return _func_get_single_state(full_trajectory)
+            return get_state(full_trajectory, time=-1)
 
     def run_ensemble(
         self,
@@ -125,7 +121,7 @@ class DynamicalModel(ABC):
         return_tlm: bool,
     ) -> list[xr.Dataset] | xr.Dataset:
         """Run an ensemble (or a single run) of the numerical model."""
-        if (self.ensemble_size == 1) and return_tlm:
+        if (self.ensemble_size > 1) and return_tlm:
             raise ValueError("TLM / Adjoint model not supported for ensembles at the moment.")
         # also handles single model run
         if self.ensemble_size == 1:
@@ -194,7 +190,8 @@ class DynamicalModel(ABC):
             state_xr, linear_xr = state_xr
             # set the new linearized matrix (TLM)
             updated_linear = np.asarray(linear_xr.isel(time=-1).data)
-            self.linear = updated_linear
+
+            self._linear = updated_linear
 
         # add noise to new state
         noisy_state_xr = add_noise(

src/ml_da/data/dynamical_models/synthetic_numerical_model.py
@@ -27,7 +27,8 @@ class SyntheticNumericalModel(DynamicalModel):
         # init system depending on name
         self.true_system = SYSTEM_REGISTRY[sys_cfg.name](sys_cfg)
         self.true_initial_state = self.true_system.initial_state
-        self.true_system_dim = self.true_system.dim
+        self.true_system_dim = self.true_system.system_dim
+
         # only call super afterwards, because it calls create_initial_state
         super().__init__(dyn_cfg, return_tlm)
 
@@ -95,16 +96,16 @@ class SyntheticNumericalModel(DynamicalModel):
             raise ValueError("Expected a single state, but got an ensemble of states.")
 
         # make sure we are not running the model on our true initial state on accident
-        if np.array_equal(self.state, self.true_initial_state):
+        if np.array_equal(state, self.true_initial_state):
             raise RuntimeError(
                 "The model runs a realization that has the same state like the ground truth, i.e. something went wrong. Model runs are supposed to never be equal to the ground truth."
             )
 
         # call the system directly (not via step, to generate the tlm)
-        model_data = self.true_system._system.generate(
+        # they start with the initial states, that's why we need to add +1 here
+        # otherwise we only get the initial states for step=1
+        return self.true_system._system.generate(
             x0=state,
-            n_steps=n_steps,
+            n_steps=n_steps + 1,
             return_tlm=return_tlm,
         )
-
-        return model_data