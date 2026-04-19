f5370ed48aaa4fe7344fd732b43b4dbe733783c9
liellnima <julia.kaltenborn@mail.mcgill.ca>
2026-04-19 05:19:05 +0200

    update registry

--- src/ml_da/tools/registry.py
+++ src/ml_da/tools/registry.py
@@ -2,6 +2,9 @@ SYSTEM_REGISTRY = {}
 OBSERVER_REGISTRY = {}
 DYNAMICAL_MODEL_REGISTRY = {}
 
+DA_METHOD_REGISTRY = {}
+ML_METHOD_REGISTRY = {}
+
 
 def system(cls):
     SYSTEM_REGISTRY[cls.__name__] = cls
@@ -15,4 +18,14 @@ def observer(cls):
 
 def dynamical_model(cls):
     DYNAMICAL_MODEL_REGISTRY[cls.__name__] = cls
+    return cls
+
+
+def ml_method(cls):
+    ML_METHOD_REGISTRY[cls.__name__] = cls
+    return cls
+
+
+def da_method(cls):
+    DA_METHOD_REGISTRY[cls.__name__] = cls
     return cls
